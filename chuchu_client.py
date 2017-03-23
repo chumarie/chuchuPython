df0b8d9ef0f23967aa2b6fb88bc8a32921de90a8
chu_m <marie.chu@epitech.eu>
2017-03-23 20:24:27 +0100

    Server.py

--- chuchu_client.py
+++ chuchu_client.py
@@ -1,8 +1,111 @@
 from tkinter import *
 from tkinter import scrolledtext, messagebox
 import sys, socket, select, _thread
+ 
+#def connect(host, port):
+running = False
 
-#Interface Tkinter
+def send():
+   data = message.get()
+   #print(data)
+   try:
+      s.send(data.encode())
+   except:
+      error = messagebox.showinfo('Disconnected', 'Server is not active')
+      exit()
+   response.delete(0, END)
+   chat.config(state=NORMAL)
+   chat.insert(END ,'<You>' + data + '\n')
+   chat.see(END)
+   chat.config(state=DISABLED)
+
+def sendonreturn(event):
+   data = message.get()
+   #print(data)
+   try:
+      s.send(data.encode())
+   except:
+      error = messagebox.showinfo('Disconnected', 'Server is not active')
+      exit()
+   response.delete(0, END)
+   chat.config(state=NORMAL)
+   chat.insert(END ,'<You>' + data + '\n')
+   chat.see(END)
+   chat.config(state=DISABLED)
+
+def receive():
+   global running
+   while running:
+      try:
+         data = s.recv(4096)
+         data = data.decode()
+         if '<info>' in data:
+            #print('True')
+            ind = data.index('\n')
+            status.set('Users Online: '+data[8:ind])
+            if len(data) > ind+1:
+               chat.config(state=NORMAL)
+               chat.insert(END, data[ind+1:] + '\n')
+               chat.see(END)
+               chat.config(state=DISABLED)
+         else:
+            #print(data)
+            chat.config(state=NORMAL)
+            chat.insert(END, data + '\n')
+            chat.see(END)
+            chat.config(state=DISABLED)
+      except:
+         running = False
+      #print(history.get())
+
+
+def available():
+   s.send('<available>'.encode())
+
+def change():
+   s.send('<change>'.encode())
+
+def donothing():
+   filewin = Toplevel(root)
+   button = Button(filewin, text="Do nothing button")
+   button.pack()
+
+def webcam():
+   s.send('<webcam>'.encode())
+
+def change():
+   s.send('<change>'.encode())
+def test():
+   s.send('<test>'.encode())
+def Quit():
+   try:
+      s.send('<quit>'.encode())
+   except:
+      pass
+   s.close()
+   root.destroy()
+   global running
+   running = False
+   quit()
+
+def on_closing():
+   if messagebox.askokcancel("Quit", "Do you want to quit?"):
+      try:
+         s.send('<quit>'.encode())
+      except:
+         pass
+      s.close()
+      root.destroy()
+
+
+host = '127.0.0.1'
+port = 6000
+flag = 1
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#except :
+#   return 'Unable to connect'''
+
 root = Tk()
 frame1 = Frame(root)
 #frame2 = Frame(root)
@@ -15,28 +118,41 @@ status = StringVar()
 
 menubar = Menu(root)
 filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label = 'Connect')
-filemenu.add_command(label = 'Disconnect')
-menubar.add_cascade(label="Statut", menu=filemenu)
+filemenu.add_command(label="Exit", command=Quit)
+menubar.add_cascade(label="File", menu=filemenu)
 optionsmenu = Menu(menubar, tearoff=0)
-optionsmenu.add_command(label = 'Create new room')
-optionsmenu.add_command(label="Change Chat Room")
-optionsmenu.add_command(label = 'Users in my room')
-optionsmenu.add_command(label = 'Show webcam')
+optionsmenu.add_command(label = 'Qui est dans cette room ?', command=available)
+optionsmenu.add_command(label="Change Chat Room", command=change)
+optionsmenu.add_command(label="active webcam", command=webcam)
+optionsmenu.add_command(label="test webcam", command=test)
 menubar.add_cascade(label="Options", menu=optionsmenu)
 root.config(menu=menubar)
 menubar.config(background="pink")
 
-chat = scrolledtext.ScrolledText(frame1 ,height=10, width=80, state=DISABLED)
+chat = scrolledtext.ScrolledText(frame1, height=10, width=80, state=DISABLED)
 chat.pack(side=TOP, fill=BOTH, expand=1)
 response = Entry(frame1, textvariable = message)
 response.focus()
-send = Button(frame1, text='Envoyer')
+send = Button(frame1, text='Send', command=send)
 response.pack(side=LEFT, fill=X, expand=1)
 send.pack(side=RIGHT)
 online_users = Label(root, textvariable = status, relief = SUNKEN, anchor = W)
 online_users.pack(side = BOTTOM, fill = X, expand = 1)
-root.bind('<Return>')
-root.protocol("WM_DELETE_WINDOW")
-
+root.bind('<Return>', sendonreturn)
+root.protocol("WM_DELETE_WINDOW", on_closing)
+if flag == 1:
+   try :
+      s.connect((host, port))
+   except:
+      error = messagebox.showinfo('Unable to Connect', 'Server is not active')
+      root.destroy()
+      _thread.interrupt_main()
+      quit()
+   running = True
+   _thread.start_new_thread(receive, ())
+   status.set('Users Online: ')
+   flag+=1
 root.mainloop()
+
+
+
--- chuchu_server.py
+++ chuchu_server.py
@@ -1,1 +1,222 @@
-Server
+#Import librairie
+import socket
+import select
+import _thread
+import numpy as np
+import cv2
+
+
+#Initialisation
+host = '127.0.0.1'
+port = 6000
+sock_list = []
+player_maps = {}
+history = ['Room1:']
+capArray = []
+
+#Init socket
+server_socket = socket.socket()
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((host, port))
+server_socket.listen(30)
+sock_list.append(server_socket)
+
+def broadcast(data, sock, group):
+    for socket in sock_list:
+        if socket != sock and socket != server_socket:
+            if player_maps[socket][0] == group:
+                try:
+                    socket.send(('<'+player_maps[sock][1]+'> '+data).encode())
+                except:
+                    socket.close()
+                    if socket in sock_list:
+                        user = player_maps[socket][1]
+                        del player_maps[socket]
+                        broadcast_to_all()
+                        sock_list.remove(socket)
+                        broadcast_offline(user, group)
+
+def broadcast_offline(user, group):
+    for socket in sock_list:
+        if socket != server_socket:
+            if player_maps[socket][0] == group:
+                try:
+                    socket.send(('<'+user+'> is offline').encode())
+                except:
+                    socket.close()
+                    if socket in sock_list:
+                        flag = 0
+                        if socket in player_maps.keys():
+                            user = player_maps[socket][1]
+                            del player_maps[socket]
+                            flag = 1
+                        broadcast_to_all()
+                        sock_list.remove(socket)
+                        if flag == 1:
+                            broadcast_offline(user, group)
+
+def broadcast_to_all():
+    for socket in sock_list:
+        if socket != server_socket:
+                try:
+                    socket.send(('<info>'+str(len(sock_list)-1)+'\n').encode())
+                except:
+                    socket.close()
+                    if socket in sock_list:
+                        user = player_maps[socket][1]
+                        del player_maps[socket]
+                        broadcast_to_all()
+                        sock_list.remove(socket)
+                        broadcast(user+' is offline', socket, group)
+
+def handle(conn):
+    try:
+        sock_list.append(conn)
+        broadcast_to_all()
+        roomlist = 'Listes des rooms:\n'
+        roomlist = roomlist + 'Room1 ' + 'Add Room'
+        conn.send(('Choisissez votre room\n\n'+roomlist).encode())
+        choice = conn.recv(4096).decode()
+        print(sock_list)
+        flag = 1
+        while flag:
+            try:
+                choice = int(choice)
+                flag = 0
+            except:
+                conn.send('Valeur incorrect. Veuillez réessayer.'.encode())
+                choice = conn.recv(4096).decode()
+
+        while choice > len(history)+1:
+            conn.send('Valeur incorrect. Veuillez réessayer.'.encode())
+            choice = conn.recv(4096).decode()
+            flag = 1
+            while flag:
+                try:
+                    choice = int(choice)
+                    flag = 0
+                except:
+                    conn.send('Invalid Input. Try Again.'.encode())
+                    choice = conn.recv(4096).decode()
+        #room dans history
+        if len(history)>=choice:
+            conn.send('Tapez votre pseudo'.encode())
+            username = conn.recv(4096).decode()
+            player_maps[conn] = [choice, username]
+            conn.send(history[choice-1].encode())
+            print("ok1")
+            print(history[choice-1].encode())
+            broadcast(username+' est connecté', conn, choice)
+        #new room
+        else:
+            history.append('Room'+str(choice)+':')
+            conn.send('Tapez votre pseudo'.encode())
+            username = conn.recv(4096).decode()
+            player_maps[conn] = [choice, username]
+            conn.send(history[choice-1].encode())
+            broadcast(username+' est connecté', conn, choice)
+    except:
+        if sock in sock_list:
+            sock_list.remove(conn)
+        broadcast_to_all()
+        if sock in player_maps.keys():
+            del player_maps[sock]
+
+while 1:
+    ready_to_read, ready_to_write, in_error = select.select(sock_list,[],[])
+    for sock in ready_to_read:
+        if sock == server_socket:
+            sockfd, addr = server_socket.accept()
+            _thread.start_new_thread(handle, (sockfd,))
+            print ("Client (%s, %s) connected" % addr)
+        else:
+            try:
+                data = sock.recv(4096).decode()
+                if data == '<change>':
+                    roomlist = 'Available rooms are:\n'
+                    prev = player_maps[sock][0]
+                    if len(history) == 1:
+                        sock.send('No other available groups to join.'.encode())
+                        continue
+                    for i in range(1, len(history)+1):
+                        if i != prev:
+                            roomlist = roomlist + 'Room' + str(i) + ' '
+                    sock.send(('Which room would you like to choose? ' + roomlist).encode())
+                    user = player_maps[sock][1]
+                    ind = sock.recv(4096).decode()
+                    print(ind)
+                    try:
+                        ind = int(ind)
+                    except:
+                        sock.send('Invalid Input. Try Again.'.encode())
+                        continue
+                    if ind > len(history):
+                        sock.send('Invalid Input. Try Again.'.encode())
+                        continue
+                    if prev == ind:
+                        sock.send('You are currently in the group you are requesting to join'.encode())
+                        continue
+                    player_maps[sock][0] = ind
+                    sock.send(history[ind-1].encode())
+                    broadcast(user+' is offline', sock, prev)
+                    broadcast(user+' is online', sock, ind)
+                elif data == '<available>':
+                    text = '('
+                    for socket in sock_list:
+                        if socket != server_socket:
+                            if player_maps[sock][0] == player_maps[socket][0]:
+                                text = text + player_maps[socket][1] + ', '
+                    text = text[:-2] + ')'
+                    sock.send(text.encode())
+                elif data == '<quit>':
+                    user = player_maps[sock][1]
+                    group = player_maps[sock][0]
+                    del player_maps[sock]
+                    sock_list.remove(sock)
+                    broadcast_to_all()
+                    sock.close()
+                    broadcast_offline(user, group)
+                elif data == '<webcam>':
+                    cap = cv2.VideoCapture(0)
+                    capArray.append(cap)
+                    while(True):
+                        ret, frame = cap.read()
+                        cv2.imshow('frame',frame)
+                        if cv2.waitKey(1) & 0xFF == ord('q'):
+                            break
+                    cap.release()
+                    cv2.destroyAllWindows()
+                elif data == '<test>':
+                    cap = capArray[0]
+                    capArray.append(cap)
+                    while(True):
+                        ret, frame = cap.read()
+                        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                        cv2.imshow('frame',gray)
+                        if cv2.waitKey(1) & 0xFF == ord('q'):
+                            break
+                    cap.release()
+                    cv2.destroyAllWindows()
+                elif data:
+                    history[player_maps[sock][0]-1] = history[player_maps[sock][0]-1]+'\n<'+player_maps[sock][1]+'> '+data
+                    broadcast(data, sock, player_maps[sock][0])
+                else:
+                    if sock in sock_list:
+                        user = player_maps[sock][1]
+                        group = player_maps[sock][0]
+                        del player_maps[sock]
+                        sock_list.remove(sock)
+                        broadcast_to_all()
+                        sock.close()
+                        broadcast_offline(user, group)
+                        #print(2)
+            except:
+                if sock in sock_list:
+                    sock_list.remove(sock)
+                if sock in player_maps.keys():
+                    user = player_maps[sock][1]
+                    group = player_maps[sock][0]
+                    broadcast_to_all()
+                    sock.close()
+                    broadcast_offline(user, group)
+                #print(3)
