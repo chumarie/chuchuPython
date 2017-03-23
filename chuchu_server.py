0e20c519f626fe0e77b0e77d237190c115440264
chu_m <marie.chu@epitech.eu>
2017-03-24 00:58:47 +0100

    Create Tkinter widgets

--- chuchu_server.py
+++ chuchu_server.py
@@ -178,12 +178,14 @@ while 1:
                     broadcast_offline(user, group)
                 elif data == '<webcam>':
                     cap = cv2.VideoCapture(0)
-                    capArray.append(cap)
-                    ret, frame = cap.read()
-                    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                    cv2.imshow('frame',gray)
-                    if cv2.waitKey(1) & 0xFF == ord('q'):
-                        cap.release()
+                    print(cap)
+                    while(True):
+                        ret, frame = cap.read()
+                        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                        cv2.imshow('frame',gray)
+                        if cv2.waitKey(1) & 0xFF == ord('q'):
+                            break
+                    cap.release()
                     cv2.destroyAllWindows()
                 elif data == '<test>':
                     print('ok')
